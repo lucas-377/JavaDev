18244b70e781f1ff2bdd5cf1dea65e71d16dcdf0
Lucas Santana <lucas_377@hotmail.com>
2020-11-27 00:21:05 -0300

    Corrigido nomenclatura do pacote dos registros.

---
+++ oficina/src/main/java/br/com/mjv/oficina/registros/controller/registrosController.java
@@ -0,0 +1,21 @@
+package br.com.mjv.oficina.registros.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * @author B4TMAN
+ * 
+ * Controller para a requisição da url de registro de defeitos veiculares.
+ *
+ */
+
+@Controller
+public class registrosController {
+
+	@RequestMapping("/registros")
+	public String registros() {
+		return "registros";
+	}
+}
